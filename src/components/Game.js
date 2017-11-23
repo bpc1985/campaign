84bf80356f837fd5ab576a73f5b70265357bdfec
Hung Ho <hung.ho@vertoanalytics.com>
2017-11-23 23:05:38 +0200

    Update header

--- src/components/Game.js
+++ src/components/Game.js
@@ -159,13 +159,13 @@ class Game extends Component {
         <div className="row">
           <div className="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
             <p>
-              IT-tiimimme TietoLepällä päätti nostaa toimistomme tunnelmaa uusilla vedenkestävillä JBL Charge 3 -kaiuttimilla, mutta tilausprosessissa kävikin kömmähdys ja pöydällemme jäikin yksi kaiutin ylimääräiseksi.
+              IT-tiimimme TietoLepällä päätti nostaa toimistomme tunnelmaa uusilla vedenkestävillä JBL Charge 2 -kaiuttimilla, mutta tilausprosessissa kävikin kömmähdys ja pöydällemme jäikin yksi kaiutin ylimääräiseksi.
             </p>
             <p>
               Päätimme tehdä onnen onnettomuudesta, ja pistää arvonnan pystyyn huisin hauskan kyselyn kautta.
             </p>
             <p>
-              <b>Osallistu skabaan ja meidän toimistoakin koristava hieno JBL Charge 3 kaiutin voi olla sinun!</b>
+              <b>Osallistu skabaan ja meidän toimistoakin koristava hieno JBL Charge 2 kaiutin voi olla sinun!</b>
             </p>
             <hr />
           </div>
