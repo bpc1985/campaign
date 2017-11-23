e03b7c810de6400a370f5ad8df9d6c44d3793f3a
Hung Ho <hung.ho@vertoanalytics.com>
2017-11-23 21:44:25 +0200

    Handle popup form

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
 import $ from 'jquery';
+import firebase from '../firebase';
 
 class Popup extends Component {
   constructor() {
     super();
+    this.state = {
+      email: ''
+    };
   }
 
   componentDidMount() {
     $('#popupModal').modal('show');
   }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const {email} = this.state;
+    const newslettersRef = firebase.database().ref('newsletters');
+    const emailInfo = {email};
+    newslettersRef.push(emailInfo);
+    $('#popupModal').modal('toggle');
+  };
 
   render() {
     return (
@@ -19,8 +38,13 @@ class Popup extends Component {
                     <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                     <h2>Älä mene vielä!</h2>
                     <p>Jos tilaat uutiskirjeemme, saat valitsemastasi palvelusta 15% alennusta!</p>
-                    <form id="popupForm" name="popupForm">
-                        <input type="email" name="email" placeholder="Sähköpostiosoitteesi" />
+                    <form id="popupForm" name="popupForm" onSubmit={this.handleSubmit}>
+                        <input
+                          type="email"
+                          name="email"
+                          placeholder="Sähköpostiosoitteesi"
+                          onChange={this.handleChange}
+                          value={this.state.email} />
                         <input type="submit" name="submit" value="Lähetä" />
                     </form>
                 </div>
